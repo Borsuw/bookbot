cd6a8b428156a42ca63e22718dc98d1ad34e6701
Borsuw <aolesiewicz@icloud.com>
2025-02-27 14:39:38 +0100

    add ability to use arguments

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+import sys
 from stats import get_book_text, count_words, count_characters, sort_char_dict
 
 def main():
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
     book_text = get_book_text(book_path)
     word_count = count_words(book_text)
     char_list = sort_char_dict(count_characters(book_text))
