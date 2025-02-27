b4bf60d6eb864cc4819147a6f72411d4d7ae6577
Borsuw <aolesiewicz@icloud.com>
2025-02-27 15:27:54 +0100

    small outpput formatting changes

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from stats import get_book_text, count_words, count_characters, sort_char_dict
 
 def main():
     if len(sys.argv) != 2:
-        print("Usage: python3 main.py <path_to_book>")
+        print("Usage: python3 main.py <relative path to book>")
         sys.exit(1)
     book_path = sys.argv[1]
     book_text = get_book_text(book_path)
@@ -11,12 +11,12 @@ def main():
     char_list = sort_char_dict(count_characters(book_text))
     print(f"============ BOOKBOT ============\n"
           f"Analyzing book found at {book_path}\n"
-          f"----------- Word Count ----------\n"
+          f"------------ Word Count ------------\n"
           f"Found {word_count} total words\n"
-          f"--------- Character Count -------")
+          f"------------ Character Count ------------")
     for char_dict in char_list:
         for char, number in char_dict.items():
             print(f"{char}: {number}")
-    print("============= END ===============")
+    print("============ END ============")
 
 main()
