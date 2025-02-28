65c685212876cc4efb3ddcbd3face405d85ff2fa
Borsuw <aolesiewicz@icloud.com>
2025-02-28 13:33:08 +0100

    separate generating the report into its own function

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 import sys
 from stats import get_book_text, count_words, count_characters, sort_char_dict
 
+def generate_report(book_path, word_count, char_list):
+    report = (f"============ BOOKBOT ============\n"
+              f"Analyzing book found at {book_path}\n"
+              f"------------ Word Count ------------\n"
+              f"Found {word_count} total words\n"
+              f"------------ Character Count ------------\n")
+    for char_dict in char_list:
+        char, number = list(char_dict.items())[0]
+        report += f"{char}: {number}\n"
+    report += "============ END ============\n"
+    return report
+
 def main():
     if len(sys.argv) != 2:
         print("Usage: python3 main.py <relative path to book>")
@@ -9,28 +21,13 @@ def main():
     book_text = get_book_text(book_path)
     word_count = count_words(book_text)
     char_list = sort_char_dict(count_characters(book_text))
-    print(f"============ BOOKBOT ============\n"
-          f"Analyzing book found at {book_path}\n"
-          f"------------ Word Count ------------\n"
-          f"Found {word_count} total words\n"
-          f"------------ Character Count ------------")
-    for char_dict in char_list:
-        char, number = list(char_dict.items())[0]
-        print(f"{char}: {number}")
-    print("============ END ============")
+    report = generate_report(book_path, word_count, char_list)
+    print(report)
     print("Would you like to print the output to a file? (y/n)")
     user_input = input().strip().lower()
     if user_input in ["y", "yes"]:
         with open("report.txt", "w") as f:
-            f.write(f"============ BOOKBOT ============\n"
-                    f"Analyzing book found at {book_path}\n"
-                    f"------------ Word Count ------------\n"
-                    f"Found {word_count} total words\n"
-                    f"------------ Character Count ------------\n")
-            for char_dict in char_list:
-                char, number = list(char_dict.items())[0]
-                f.write(f"{char}: {number}\n")
-            f.write("============ END ============\n")
+            f.write(report)
         print("Output written to report.txt, exiting...")
     else:
         print("No report saved, exiting...")
